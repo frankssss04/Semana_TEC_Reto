be26452a6f08ea564471bd70928e0706a6f37db3
a01786948-debug <a01786948@tec.mx>
2025-09-18 22:33:49 -0600

    Add cambios a

--- cannon.py
+++ cannon.py
@@ -23,8 +23,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 200) / 20
+        speed.y = (y + 200) / 20
 
 
 def inside(xy):
@@ -55,10 +55,10 @@ def move():
         targets.append(target)
 
     for target in targets:
-        target.x -= 0.5
+        target.x -= 2.5
 
     if inside(ball):
-        speed.y -= 0.35
+        speed.y -= 0.5
         ball.move(speed)
 
     dupe = targets.copy()
@@ -72,7 +72,10 @@ def move():
 
     for target in targets:
         if not inside(target):
-            return
+            target.x = 200
+            target.y = randrange(-150, 150) 
+
+        
 
     ontimer(move, 50)
 
